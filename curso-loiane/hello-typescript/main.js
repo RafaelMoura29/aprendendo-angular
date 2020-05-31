f6d0ddad9cd3f7cf67096bb5efbba6109e920846
Rafael Moura <rafaelmoura2929@gmail.com>
2020-05-31 12:48:51 -0300

    introdução ao typescript para angular

---
+++ curso-loiane/hello-typescript/main.js
@@ -0,0 +1,18 @@
+var minhaVar = "minha variavel";
+function minhaFunc(x, y) {
+    return x + y;
+}
+var num = 1;
+var PI = 3.14;
+var numeros = [1, 2, 3];
+numeros.map(function (num) { return num * 2; });
+var Matematica = /** @class */ (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
+var n1 = 'dasdsa';
+n1 = 2;
---
+++ curso-loiane/hello-typescript/main.ts
@@ -0,0 +1,20 @@
+var minhaVar = "minha variavel"
+
+function minhaFunc(x, y) {
+    return x + y
+}
+
+let num = 1
+const PI = 3.14
+
+var numeros = [1, 2, 3]
+
+numeros.map(num => num * 2)
+
+class Matematica {
+    soma(x, y) {
+        return x + y
+    }
+}
+var n1: any = 'dasdsa'
+n1 = 2

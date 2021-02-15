7117dee85f18d5d90d712870f80afcf9e8f4db11
rafaelmoura29 <rafaelmoura2929@gmail.com>
2021-02-15 19:16:31 -0300

    checkpoint diretivas

---
+++ curso-loiane/diretivas/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { Teste2Component } from './teste2/teste2.component';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DiretivaNgifComponent,
+    Teste2Component,
+    DiretivaNgswitchComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ curso-loiane/diretivas/src/app/teste2/teste2.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teste2',
+  templateUrl: './teste2.component.html',
+  styleUrls: ['./teste2.component.css']
+})
+export class Teste2Component implements OnInit {
+
+  minhaVariavel: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

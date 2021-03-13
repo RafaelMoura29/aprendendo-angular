fc8a84c0135b6b68594ffc599a64487daacde831
Rafael Serafim <rafaelserafim@wizsolucoes.com.br>
2021-03-13 16:48:14 -0300

    checkpoint: pipes

--- curso-loiane/pipes/src/app/app.module.ts
+++ curso-loiane/pipes/src/app/app.module.ts
@@ -1,16 +1,34 @@
+import { SettingsService } from './settings.service';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
+import { CamelCasePipe } from './camel-case.pipe';
+import {registerLocaleData} from '@angular/common';
+import br from '@angular/common/locales/pt';
+registerLocaleData(br, 'pt-BR');
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExemplosPipesComponent,
+    CamelCasePipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    /* {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    } */
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: (SettingsService) => SettingsService.getLocale()
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
